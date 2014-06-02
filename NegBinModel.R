01c82e02c4ac72f77719a02a8c50f989d9a0028b
ntyet <ntyet@iastate.edu>
2014-06-02 12:32:25 -0500

    model works!

--- NegBinModel.R
+++ NegBinModel.R
@@ -11,7 +11,7 @@ log(lambda[i]) <- alpha[gene[i]] + (-1)^line[i]*tau[gene[i]] + beta[gene[i]]*cov
 # prior level 1
 for (i in 1:ngene){
 alpha[i] ~ dnorm(0,1/10000)
-log(omega[i]) ~ dnorm(0, 1/10000)
+omega[i] ~ dlnorm(0, 1/10000)
 tau[i] <- (1-bintau[i])*normtau[i]
 bintau[i] ~ dbern(pitau)
 normtau[i] ~ dnorm(0,1/sigmatau^2)
@@ -56,20 +56,23 @@ library(reshape)
 sim_data <- function(K, ngene, mualpha, sigmaalpha, 
                      pitau,mutau, sigmatau, pibeta,
                      mubeta, sigmabeta){
-  # prior level 1
+  # prior level 1 
   x <- rnorm(2*K, 0,1)
   bintau <- rbinom(ngene,1,pitau)
   tau <-(1-bintau)*rnorm(ngene, mutau, sigmatau)
-  beta <- (1-rbinom(ngene,1,pibeta))*rnorm(ngene, mubeta, sigmabeta)
+  binbeta <- rbinom(ngene,1,pibeta)
+  beta <- (1-binbeta)*rnorm(ngene, mubeta, sigmabeta)
   alpha <- rnorm(ngene, mualpha,sigmaalpha )
   lambda <- matrix(0, ncol = 2*K, nrow = ngene)
+  omega <- exp(rnorm(ngene, 0, 2))
   count <- matrix(0, ncol = 2*K, nrow = ngene)
+  
   for (j in 1:ngene){
     for (k in 1:K){
       lambda[j,k] <- exp(alpha[j] - tau[j] + beta[j]*x[k])
       lambda[j, k+K] <- exp(alpha[j] + tau[j] + beta[j]*x[k+K])
-      count[j,k] <- rpois(1, lambda[j,k])
-      count[j,k+K] <- rpois(1, lambda[j,k+K])
+      count[j,k] <- rnbinom(1, size = 1/omega[j], mu = lambda[j,k])
+      count[j,k+K] <- rnbinom(1, size = 1/omega[j], mu = lambda[j,k+K])
     }
   }
   melt_count <- melt(count)
@@ -83,9 +86,12 @@ sim_data <- function(K, ngene, mualpha, sigmaalpha,
               line = melt_count$line, 
               cov = melt_count$cov, 
               ngene = ngene,
+              bintau=bintau,
               tau = tau, 
+              binbeta = binbeta,
               beta = beta, 
-              alpha = alpha)
+              alpha = alpha, 
+              omega = omega)
   return(dat)
   
 }
@@ -115,30 +121,30 @@ sim_data <- function(K, ngene, mualpha, sigmaalpha,
     mm_tau_correct <- sum(mm_tau_est%in%mm_tau_true)
     mm_tau_correct_eps <- sum(mm_tau_est_eps%in%mm_tau_true)
     
-    mh <- jags.model(textConnection(modelh), data[1:5] ,n.chains = 1)
-    resh <- coda.samples(mh, c("tau","alpha","beta",
-                               "sigmatauj", "sigmabetaj",  
-                               "sigmatau","sigmabeta"), 2000) 
+#    mh <- jags.model(textConnection(modelh), data[1:5] ,n.chains = 1)
+#    resh <- coda.samples(mh, c("tau","alpha","beta",
+#                               "sigmatauj", "sigmabetaj",  
+#                               "sigmatau","sigmabeta"), 2000) 
     
     
-    mh_tau_est <- which(apply(resh[[1]][,paste("sigmatauj[", 1:ngene,"]",sep ="")], 2, 
-                              function(x) mean(1-1/(x^2+1))) > 0.5)
+#    mh_tau_est <- which(apply(resh[[1]][,paste("sigmatauj[", 1:ngene,"]",sep ="")], 2, 
+#                              function(x) mean(1-1/(x^2+1))) > 0.5)
     
-    mh_tau_est_eps <- which(apply(resh[[1]][,paste("tau[", 1:ngene,"]",sep ="")], 2, 
-                                  function(x) mean(abs(x)>epstau)) > 0.5)
-    mh_tau_true <- which(data$tau!=0)
-    mh_tau_correct <- sum(mh_tau_est%in%mh_tau_true)
-    mh_tau_correct_eps <- sum(mh_tau_est_eps%in%mh_tau_true)
+#    mh_tau_est_eps <- which(apply(resh[[1]][,paste("tau[", 1:ngene,"]",sep ="")], 2, 
+#                                  function(x) mean(abs(x)>epstau)) > 0.5)
+#    mh_tau_true <- which(data$tau!=0)
+#    mh_tau_correct <- sum(mh_tau_est%in%mh_tau_true)
+#    mh_tau_correct_eps <- sum(mh_tau_est_eps%in%mh_tau_true)
     
     return(c(   mm_tau_est = length(mm_tau_est), 
                 mm_tau_correct_est = mm_tau_correct,
-                mh_tau_est = length(mh_tau_est), 
-                mh_tau_correct_est = mh_tau_correct,
+#                mh_tau_est = length(mh_tau_est), 
+ #               mh_tau_correct_est = mh_tau_correct,
                 
                 mm_tau_est_eps = length(mm_tau_est_eps), 
                 mm_tau_correct_est_eps = mm_tau_correct_eps,
-                mh_tau_est_eps = length(mh_tau_est_eps), 
-                mh_tau_correct_est_eps = mh_tau_correct_eps,
+  #              mh_tau_est_eps = length(mh_tau_est_eps), 
+   #             mh_tau_correct_est_eps = mh_tau_correct_eps,
                 
                 tau_true = length(mm_tau_true)
     ))
@@ -154,10 +160,12 @@ ngene <- 100
 mualpha <- 3
 sigmaalpha <- 2
 pitau <-  0.8
-mutau <- c(0.5,1,2)
+#mutau <- c(0.5,1,2)
+mutau <- 1
 sigmatau <- 0.25
 pibeta <- 0.8
-mubeta <- c(0.5,1,2)
+#mubeta <- c(0.5,1,2)
+mubeta <- 1
 sigmabeta  <- 0.25
 epstau <- epsbeta <- 2*mutau/3
 post_out <- array(0, dim = c(3,3,9))
